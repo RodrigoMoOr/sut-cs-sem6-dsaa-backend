717b584977cfcfc08044a4bc706548a5b14dd8e4
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-03-31 12:30:38 +0200

    :fire: added feature mpdules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { PaymentModule } from './modules/payment/payment.module';
+import { CartModule } from './modules/cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, PaymentModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
