468a52bf77fe39ea6b3c2b1505085cccd52bc7c8
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-01 11:40:46 +0200

    :fire: added books controller in library module

--- src/modules/library/library.module.ts
+++ src/modules/library/library.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { BooksController } from './controllers/books/books.controller';
 
-@Module({})
+@Module({
+  controllers: [BooksController]
+})
 export class LibraryModule {}
