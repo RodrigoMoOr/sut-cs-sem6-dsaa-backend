4b01a15304dcdd543b25deff0d39eadcc4c265a5
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-01 11:39:49 +0200

    :fire: added library module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { PaymentModule } from './modules/payment/payment.module';
 import { CartModule } from './modules/cart/cart.module';
+import { LibraryModule } from './modules/library/library.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PaymentModule, CartModule],
+  imports: [AuthModule, UserModule, PaymentModule, CartModule, LibraryModule],
   controllers: [AppController],
   providers: [AppService],
 })
