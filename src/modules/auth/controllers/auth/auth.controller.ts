3108f50f42f763c8776204ecd2387e069242ebf0
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-04 12:01:14 +0200

    :fire: added login DTO and method in auth controller class

--- src/modules/auth/controllers/auth/auth.controller.ts
+++ src/modules/auth/controllers/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from '../../dto/create-user.dto';
+import { LoginDto } from '../../dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post()
+  createUser(@Body() user: CreateUserDto): void {}
+
+  @Post()
+  login(@Body() credentials: LoginDto): void {}
+}
