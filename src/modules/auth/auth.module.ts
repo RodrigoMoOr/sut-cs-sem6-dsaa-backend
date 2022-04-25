8050fab7c9f09eb744c9d1ea3eec1119975218df
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-25 13:59:02 +0200

    :construction: added auth service and updated auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
+import { AuthService } from './services/auth/auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
