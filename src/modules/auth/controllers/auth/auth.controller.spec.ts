a9f7c7c025c218a6b3e6a701101a0ad424481c2d
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-25 13:25:00 +0200

    :hammer: updated lint-staged
    :white_check_mark: updated auth controller test

--- src/modules/auth/controllers/auth/auth.controller.spec.ts
+++ src/modules/auth/controllers/auth/auth.controller.spec.ts
@@ -1,13 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
-import { AuthService } from '../../services/auth/auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [],
       controllers: [AuthController],
     }).compile();
 
