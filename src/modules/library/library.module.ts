2db4280d59321074a0221dd31871411dc0adff38
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-25 23:00:59 +0200

    :beers: :construction: added genre controller and updated library module

--- src/modules/library/library.module.ts
+++ src/modules/library/library.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './controllers/books/books.controller';
 import { AuthorsController } from './controllers/authors/authors.controller';
+import { GenreController } from './controllers/genre/genre.controller';
 
 @Module({
-  controllers: [BooksController, AuthorsController]
+  controllers: [BooksController, AuthorsController, GenreController]
 })
 export class LibraryModule {}
