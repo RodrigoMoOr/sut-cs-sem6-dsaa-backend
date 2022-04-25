4df84e9876acbd41ffa24de890ddfc42c5a54710
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-25 14:06:41 +0200

    :sparkles: added user validation method

--- src/modules/auth/services/auth/auth.service.ts
+++ src/modules/auth/services/auth/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../../../user/services/user/user.service';
+import { Credentials } from '../../interfaces/auth.interface';
+import { User } from '../../../user/interfaces/user.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {
+  }
 
-  // async validateUser();
+  async validateUser(credentials: Credentials): Promise<User | undefined> {
+    const user = await this.userService.findOne(credentials.email);
+
+    if (user && user.password == credentials.password) {
+      return user;
+    }
+
+    return null;
+  }
 }
