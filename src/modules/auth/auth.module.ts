9126aa7796d984c60289874bc01299db00e461b5
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-25 14:02:30 +0200

    :construction: updated imports in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
-import { AuthService } from './services/auth/auth.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/modules/auth/services/auth/auth.service.ts
+++ src/modules/auth/services/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../../../user/services/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  // async validateUser();
+}
