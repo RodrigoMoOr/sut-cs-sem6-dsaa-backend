f6f8f409b76d8c4620b49ffe09847d293b6c5672
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-26 13:13:20 +0200

    :sparkles: completed genre service with find one and find all methods

--- src/modules/library/services/genre/genre.service.ts
+++ src/modules/library/services/genre/genre.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Genre } from '../../entities/genre.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class GenreService {}
+export class GenreService {
+  constructor(@InjectRepository(Genre) private readonly genreRepository: Repository<Genre>) {
+  }
+
+  findOne(id: number): Promise<Genre> {
+    return this.genreRepository.findOne(id);
+  }
+
+  findAll(): Promise<Genre[]> {
+    return this.genreRepository.find();
+  }
+}
