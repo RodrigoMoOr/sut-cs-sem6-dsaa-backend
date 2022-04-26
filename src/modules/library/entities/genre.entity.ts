13e3bd7c1634b2c8ef3d8e6a3a9779e4cb4dc1fc
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-26 13:06:40 +0200

    :sparkles: finished genre entity

--- src/modules/library/entities/genre.entity.ts
+++ src/modules/library/entities/genre.entity.ts
@@ -1,6 +1,13 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('entity')
 export class Genre {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
 }
