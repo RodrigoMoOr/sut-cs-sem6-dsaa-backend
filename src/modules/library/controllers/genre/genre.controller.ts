a15ad5bc03c03eb6732462e59a76678804fda136
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-26 13:19:19 +0200

    :sparkles: added get genre by id and get all genres in genre controller

--- src/modules/library/controllers/genre/genre.controller.ts
+++ src/modules/library/controllers/genre/genre.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { GenreService } from '../../services/genre/genre.service';
+import { Genre } from '../../entities/genre.entity';
 
-@Controller('genre')
-export class GenreController {}
+@Controller('Genre')
+export class GenreController {
+  constructor(private readonly genreService: GenreService) {}
+
+  @Get(':id')
+  async getGenreById(@Param('id') id: number): Promise<Genre> {
+    return this.genreService.findOne(id);
+  }
+
+  @Get()
+  async getAllGenres(): Promise<Genre[]> {
+    return this.genreService.findAll();
+  }
+}
