cb40cbcdc284dac8aed4777fe2eda5255a4f2d1e
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-26 12:45:26 +0200

    :construction: added genre service WIP

--- src/modules/library/library.module.ts
+++ src/modules/library/library.module.ts
@@ -5,10 +5,11 @@ import { GenreController } from './controllers/genre/genre.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
 import { BookService } from './services/book/book.service';
+import { GenreService } from './services/genre/genre.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book])],
   controllers: [BooksController, AuthorsController, GenreController],
-  providers: [BookService],
+  providers: [BookService, GenreService],
 })
 export class LibraryModule {}
