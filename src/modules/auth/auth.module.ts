b108b4e517647dfe5543bb953508185c301cb3c9
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-28 20:44:26 +0200

    :recycle: updated auth module with GoogleAuthStrategy provider

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,11 +6,12 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JWTStrategy } from './strategies/jwt.strategy';
+import { GoogleAuthStrategy } from './strategies/google-auth.strategy';
 
 @Module({
   imports: [UserModule, PassportModule, JwtModule.register({ secret: 'SECRET', signOptions: { expiresIn: '60s' } })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JWTStrategy],
+  providers: [AuthService, LocalStrategy, JWTStrategy, GoogleAuthStrategy],
   exports: [JWTStrategy],
 })
 export class AuthModule {}
