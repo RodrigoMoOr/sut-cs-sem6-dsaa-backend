1da225844a6ffb775314a015c622606db12a123c
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-08 10:04:41 +0200

    :construction: added DTO properties, added query param to endpoint

--- src/modules/library/controllers/books/books.controller.ts
+++ src/modules/library/controllers/books/books.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { BookService } from '../../services/book/book.service';
 import { Book } from '../../entities/book.entity';
+import { GetFilteredBooksDTO } from '../../dto/get-filtered-books.dto';
 
 @ApiTags('Books')
 @Controller('books')
@@ -14,7 +15,7 @@ export class BooksController {
   }
 
   @Get()
-  async getAllBooks(): Promise<Book[]> {
+  async getBooks(@Query() filtersDTO: GetFilteredBooksDTO): Promise<Book[]> {
     return this.bookService.findAll();
   }
 }
