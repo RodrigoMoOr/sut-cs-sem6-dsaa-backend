6ac48519e394bb76af2229dc2bfd1546bd18d604
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-08 10:13:45 +0200

    :sparkles: added method to get filted books in book service

--- src/modules/library/services/book/book.service.ts
+++ src/modules/library/services/book/book.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from '../../entities/book.entity';
 import { Repository } from 'typeorm';
+import { GetFilteredBooksDTO } from '../../dto/get-filtered-books.dto';
 
 @Injectable()
 export class BookService {
@@ -13,5 +14,17 @@ export class BookService {
 
   findAll(): Promise<Book[]> {
     return this.bookRepository.find();
+  }
+
+  async findByFilter(filters: GetFilteredBooksDTO): Promise<Book[]> {
+    const { title } = filters;
+
+    let books = await this.findAll();
+
+    if (title) {
+      books = books.filter(book => book.title === title);
+    }
+
+    return books;
   }
 }
