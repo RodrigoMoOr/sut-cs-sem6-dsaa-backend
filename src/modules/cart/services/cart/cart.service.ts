1819c5bfa32ba2a494bc66eb66e0c8e571b3bd71
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-09 23:39:52 +0200

    :construction: added create cart DTO
    :sparkles: finished create method in cart service

--- src/modules/cart/services/cart/cart.service.ts
+++ src/modules/cart/services/cart/cart.service.ts
@@ -2,13 +2,14 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cart } from '../../entities/cart.entity';
 import { Repository } from 'typeorm';
+import { CreateCartDto } from '../../../user/dto/create-cart.dto';
 
 @Injectable()
 export class CartService {
   constructor(@InjectRepository(Cart) private readonly cartRepository: Repository<Cart>) {}
 
-  async create(cart): Promise<Cart> {
-    const createdCart = this.cartRepository.create();
+  async create(cart: CreateCartDto): Promise<Cart> {
+    const createdCart = this.cartRepository.create(cart);
 
     if (!createdCart) throw new HttpException('Error creating cart for user', HttpStatus.SERVICE_UNAVAILABLE);
 
