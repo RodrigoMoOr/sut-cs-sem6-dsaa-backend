0391d35d6cad5225ce4fcbe010280df5ed6aa4c2
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-09 23:32:15 +0200

    :construction: added create method in cart service

--- src/modules/cart/services/cart/cart.service.ts
+++ src/modules/cart/services/cart/cart.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cart } from '../../entities/cart.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(@InjectRepository(Cart) private readonly cartRepository: Repository<Cart>) {}
+
+  async create(cart): Promise<Cart> {
+    const createdCart = this.cartRepository.create();
+
+    if (!createdCart) throw new HttpException('Error creating cart for user', HttpStatus.SERVICE_UNAVAILABLE);
+
+    return createdCart;
+  }
+}
