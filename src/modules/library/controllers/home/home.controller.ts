647a784ba7eea6bc02e250661dfce0a7f8c03e0e
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-23 23:33:46 +0200

    :construction: added dependency injection to home controller

--- src/modules/library/controllers/home/home.controller.ts
+++ src/modules/library/controllers/home/home.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import {Controller} from '@nestjs/common';
+import {ApiTags} from "@nestjs/swagger";
+import {AuthorService} from "../../services/author/author.service";
+import {BookService} from "../../services/book/book.service";
+import {GenreService} from "../../services/genre/genre.service";
 
+@ApiTags('Home')
 @Controller('home')
-export class HomeController {}
+export class HomeController {
+  constructor(private readonly authorService: AuthorService, private readonly bookService: BookService, private readonly genreService: GenreService) {
+  }
+}
