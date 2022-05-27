c61ee2db144c430986d00d82d3ee77f569a23e0b
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-27 22:14:07 +0200

    :contruction: added pagination functionality to genre service, WIP missing cntrlr fn

--- src/modules/library/services/genre/genre.service.ts
+++ src/modules/library/services/genre/genre.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Genre } from '../../entities/genre.entity';
 import { Repository } from 'typeorm';
+import { from, map, Observable } from 'rxjs';
+import { IPaginationOptions, paginate, Pagination } from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class GenreService {
   constructor(@InjectRepository(Genre) private readonly genreRepository: Repository<Genre>) {}
 
-  findOne(id: number): Promise<Genre> {
-    return this.genreRepository.findOne(id);
+  findOne(id: number): Promise<Genre | null> {
+    return this.genreRepository.findOneOrFail(id);
   }
 
   findAll(): Promise<Genre[]> {
     return this.genreRepository.find();
+  }
+
+  findAllPaginated(options: IPaginationOptions): Observable<Pagination<Genre>> {
+    return from(paginate<Genre>(this.genreRepository, options)).pipe(map((genres: Pagination<Genre>) => genres));
   }
 }
