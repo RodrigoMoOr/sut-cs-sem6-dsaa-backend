e42c1af4e5306c43c0b5acdbd6f6529337ef9a63
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-27 22:06:51 +0200

    :contruction: added pagination functionality to author service, WIP missing cntrlr fn

--- src/modules/library/services/author/author.service.ts
+++ src/modules/library/services/author/author.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Author } from '../../entities/author.entity';
 import { Repository } from 'typeorm';
+import { IPaginationOptions, paginate, Pagination } from 'nestjs-typeorm-paginate';
+import { from, map, Observable } from 'rxjs';
 
 @Injectable()
 export class AuthorService {
   constructor(@InjectRepository(Author) private readonly authorRepository: Repository<Author>) {}
 
-  findOne(id: number): Promise<Author> {
-    return this.authorRepository.findOne(id);
+  findOne(id: number): Promise<Author | null> {
+    return this.authorRepository.findOneOrFail(id);
   }
 
   findAll(): Promise<Author[]> {
     return this.authorRepository.find();
+  }
+
+  findAllPaginated(options: IPaginationOptions): Observable<Pagination<Author>> {
+    return from(paginate<Author>(this.authorRepository, options)).pipe(map((authors: Pagination<Author>) => authors));
   }
 }
