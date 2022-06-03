ec4229b713e313ef2f242011eed74d25bf2d77d3
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-06-03 11:56:35 +0200

    :construction: implementing manual pagination + filtering + sorting WIP

--- src/modules/library/controllers/authors/authors.controller.ts
+++ src/modules/library/controllers/authors/authors.controller.ts
@@ -1,9 +1,6 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { AuthorService } from '../../services/author/author.service';
-import { Author } from '../../entities/author.entity';
-import { Observable } from 'rxjs';
-import { Pagination } from 'nestjs-typeorm-paginate';
 import { AuthorDto } from '../../dto/author.dto';
 
 @ApiTags('Authors')
@@ -20,7 +17,7 @@ export class AuthorsController {
   @ApiQuery({ name: 'page', required: false, example: 2, description: 'Requested page' })
   @ApiQuery({ name: 'limit', required: false, example: 10, description: 'Items per page' })
   @ApiOkResponse({ description: 'Fetched books from DB', type: [AuthorDto] })
-  getAllAuthors(@Query('page') page = 1, @Query('limit') limit = 10): Observable<Pagination<Author>> {
-    return this.authorService.findAllPaginated({ page, limit, route: 'localhost:3000/authors' });
+  getAllAuthors(@Query('page') page = 1, @Query('limit') limit = 10): Promise<AuthorDto[]> {
+    return this.authorService.findAllPaginated2(page, limit);
   }
 }
